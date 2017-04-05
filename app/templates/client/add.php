f6a9e3ebcc5dbf1216a6cb332aedaaf9605755de
fran <fran.blasco94@gmail.com>
2017-04-05 13:29:28 +0200

    crear formulario para crear nuevo cliente

---
+++ app/templates/client/add.php
@@ -0,0 +1,14 @@
+<form method="POST">
+	<label for="name">Nombre</label>
+    <input type="text" name="name" required
+        value="" ><br>
+	<label for="password">Contraseña</label>
+    <input type="password"  name="password"
+        value=""><br>
+	<label for="email">Email</label>
+    <input type="email" name="mail"
+    value=""><br>
+	<input type="submit" value="Crear cliente">
+</form>
+<br>
+<a href='../profile'><button>Volver al listado</button></a>
