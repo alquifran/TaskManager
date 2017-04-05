6b757076b49f7b99f637f603eab2ea15e9349fec
fran <fran.blasco94@gmail.com>
2017-04-05 12:21:16 +0200

    Login del Técnico

---
+++ app/templates/tech/login.php
@@ -0,0 +1,6 @@
+<form action="" method="POST">
+        MailTech:<input type="email" name="mail" placeholder="mail"><br>
+        Password:<input type="password" name="password" placeholder="password"><br>
+        <input type="submit" name="submit">
+
+    </form>
