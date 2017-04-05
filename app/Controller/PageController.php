92b857c4e9d5f8171f846fb2b2a1e33c1a2b03c6
fran <fran.blasco94@gmail.com>
2017-04-05 12:22:04 +0200

    Creación del perfil del técnico y modificación en el PageController para enlazar el login del mismo

--- app/Controller/PageController.php
+++ app/Controller/PageController.php
@@ -6,6 +6,7 @@ class PageController{
 	public function index(){
 		echo "<a href=Client/Login/>Client</a><br>";
 		echo "<a href=Admin/Login/>Admin</a><br>";
+		echo "<a href=Tech/Login/>Técnico</a><br>";
 	}
 	
 }
---
+++ app/templates/tech/profile.php
@@ -0,0 +1,7 @@
+Hola técnico <?=$tech->getName();?><br>
+<?=$tech->getMail();?>
+<br>
+<form method="POST">
+		<input type="submit" name="logout" value="Cerrar sesión">
+	</form>
+	<?php //include '../task/list.php';?> 
