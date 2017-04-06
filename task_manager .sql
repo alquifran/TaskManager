0910d43ecbff313271c43b73bc7438708b1236c5
Fran <fran.blasco94@gmail.com>
2017-04-06 13:55:24 +0200

    Base de datos v4

--- task_manager .sql
+++ task_manager .sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-04-2017 a las 10:00:04
+-- Tiempo de generación: 06-04-2017 a las 13:54:09
 -- Versión del servidor: 10.1.19-MariaDB
 -- Versión de PHP: 7.0.13
 
@@ -55,19 +55,42 @@ CREATE TABLE `clients` (
   `client_id` int(11) NOT NULL,
   `client_name` varchar(50) CHARACTER SET latin1 NOT NULL,
   `client_password` varchar(250) CHARACTER SET latin1 NOT NULL,
-  `client_mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+  `client_mail` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `client_alta` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `clients`
 --
 
-INSERT INTO `clients` (`client_id`, `client_name`, `client_password`, `client_mail`) VALUES
-(1, 'David', '1234', 'david@david.com'),
-(2, 'Juan', '4321', 'juan@juan.com'),
-(3, 'Ola', 'bla', 'ola@ola.com'),
-(5, 'fran', 'fran', 'fran@fran.com'),
-(8, 'Raul', 'raul', 'raul@raul.com');
+INSERT INTO `clients` (`client_id`, `client_name`, `client_password`, `client_mail`, `client_alta`) VALUES
+(1, 'David', '1234', 'david@david.com', 0),
+(2, 'Juan', '4321', 'juan@juan.com', 1),
+(3, 'Ola', 'bla', 'ola@ola.com', 0),
+(5, 'fran', 'fran', 'fran@fran.com', 1),
+(8, 'Raul', 'raul', 'raul@raul.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `client_pack`
+--
+
+DROP TABLE IF EXISTS `client_pack`;
+CREATE TABLE `client_pack` (
+  `client_pack_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `pack_id` int(11) NOT NULL,
+  `consumed_time` int(11) NOT NULL,
+  `status` varchar(20) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `client_pack`
+--
+
+INSERT INTO `client_pack` (`client_pack_id`, `client_id`, `pack_id`, `consumed_time`, `status`) VALUES
+(1, 1, 1, 0, 'active');
 
 -- --------------------------------------------------------
 
@@ -79,16 +102,17 @@ DROP TABLE IF EXISTS `packs`;
 CREATE TABLE `packs` (
   `pack_id` int(11) NOT NULL,
   `pack_name` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `pack_desc` varchar(250) COLLATE utf8_spanish_ci NOT NULL
+  `pack_desc` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
+  `pack_time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `packs`
 --
 
-INSERT INTO `packs` (`pack_id`, `pack_name`, `pack_desc`) VALUES
-(1, 'pack1', 'Descripción de este nuestro pack 1'),
-(2, 'pack2', 'Remitirse a la descripción del pack 1 sustituyendo el 1 por un 2');
+INSERT INTO `packs` (`pack_id`, `pack_name`, `pack_desc`, `pack_time`) VALUES
+(1, 'pack1', 'Pack 1 con x minutos de trabajo', 0),
+(2, 'pack2', 'Pack 2 con "y" minutos de trabajo', 0);
 
 -- --------------------------------------------------------
 
@@ -131,16 +155,17 @@ CREATE TABLE `technicians` (
   `tech_id` int(11) NOT NULL,
   `tech_name` varchar(50) CHARACTER SET latin1 NOT NULL,
   `tech_password` varchar(250) CHARACTER SET latin1 NOT NULL,
-  `tech_mail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL
+  `tech_mail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci NOT NULL,
+  `tech_alta` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `technicians`
 --
 
-INSERT INTO `technicians` (`tech_id`, `tech_name`, `tech_password`, `tech_mail`) VALUES
-(1, 'tecnico1', '1234', 'tecnico1@tecnico1.com'),
-(2, 'tecnico2', '4321', 'tecnico2@tecnico2.com');
+INSERT INTO `technicians` (`tech_id`, `tech_name`, `tech_password`, `tech_mail`, `tech_alta`) VALUES
+(1, 'tecnico1', '1234', 'tecnico1@tecnico1.com', 0),
+(2, 'tecnico2', '4321', 'tecnico2@tecnico2.com', 0);
 
 --
 -- Índices para tablas volcadas
@@ -159,6 +184,14 @@ ALTER TABLE `admins`
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`client_id`),
   ADD UNIQUE KEY `client_mail` (`client_mail`);
+
+--
+-- Indices de la tabla `client_pack`
+--
+ALTER TABLE `client_pack`
+  ADD PRIMARY KEY (`client_pack_id`),
+  ADD KEY `pack_clients_id_idx` (`client_id`),
+  ADD KEY `pack_pack_id_idx` (`pack_id`);
 
 --
 -- Indices de la tabla `packs`
@@ -196,6 +229,11 @@ ALTER TABLE `admins`
 ALTER TABLE `clients`
   MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
+-- AUTO_INCREMENT de la tabla `client_pack`
+--
+ALTER TABLE `client_pack`
+  MODIFY `client_pack_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `packs`
 --
 ALTER TABLE `packs`
@@ -209,10 +247,17 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT de la tabla `technicians`
 --
 ALTER TABLE `technicians`
-  MODIFY `tech_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `tech_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `client_pack`
+--
+ALTER TABLE `client_pack`
+  ADD CONSTRAINT `pack_clients_id` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `pack_pack_id` FOREIGN KEY (`pack_id`) REFERENCES `packs` (`pack_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `tasks`
