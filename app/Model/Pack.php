8a564e53647fa9a17ff02a2e1616c0f31ad40871
Agoralab04\Curso PHP <davidign1688@hotmail.com>
2017-04-07 12:30:43 +0200

    Añadido los metodos packById y contractPack

--- app/Model/Pack.php
+++ app/Model/Pack.php
@@ -14,7 +14,7 @@ class Pack
 		$this->name = $name;
 		$this->desc = $desc;
 		$this->pack_time = $pack_time;
-		$this->id = $id;	
+		$this->id = $id;
 	}
 
 	public static function listPacks(){
@@ -37,13 +37,13 @@ class Pack
 	public static function packByClientId($client_id){
 		$db = Database::getInstance();
 		$req = $db->query("
-			SELECT * FROM `clients` 
-			JOIN `client_pack` 
-			ON `clients`.`client_id` = `client_pack`.`client_id` 
-			JOIN `packs` 
-			ON `client_pack`.`pack_id` = `packs`.`pack_id` 
+			SELECT * FROM `clients`
+			JOIN `client_pack`
+			ON `clients`.`client_id` = `client_pack`.`client_id`
+			JOIN `packs`
+			ON `client_pack`.`pack_id` = `packs`.`pack_id`
 			WHERE `clients`.`client_id` = '$client_id'");
-		
+
 
 		if($req->rowCount()!=0){
 			$pack = $req->fetch();
@@ -54,8 +54,30 @@ class Pack
 		}
 	}
 
+	public function packById($pack_id){
+		$db = Database::getInstance();
+		$req = $db->query("SELECT * FROM packs WHERE pack_id = $pack_id");
+
+		if($req->rowCount()!=0){
+			$pack = $req->fetch();
+			return new Pack($pack['pack_name'], $pack['pack_desc'], $pack['pack_time'], $pack['pack_id']);
+		}
+		else{
+			return null;
+		}
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
 	public function getName(){
 		return $this->name;
+	}
+
+	public static function contractPack($pack_id,$client_id){
+		$db = Database::getInstance();
+		$req = $db->query("INSERT INTO client_pack (pack_id,client_id) VALUES ($pack_id,$client_id)");
 	}
 
 
