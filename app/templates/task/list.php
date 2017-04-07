71bd73dcee5071c4473c93360031a46221e1ff84
fran <fran.blasco94@gmail.com>
2017-04-07 10:41:43 +0200

    Modificaciones a templates de clientes, tarea y técnico.

--- app/templates/task/list.php
+++ app/templates/task/list.php
@@ -1,11 +1,19 @@
-<?php
-	echo "<ul>";
-	foreach $list as $task{
-		
-		echo "<li>";
-		echo $task->getName();
-		echo "</li>"
+<table>
 
-
-	}
-	echo "</ul>";
+	<?php foreach ($tasks as $task):?>
+		<tr>
+			<td>
+				<?=$task->getName();?>
+			</td>
+			<td>
+				<a href="../showClient/<?=$task->getClientId()?>" target="_blank"><?=$task->getClient()->getName();?></a>
+			</td>
+			<td>
+				<a href="../showTech/<?=$task->getTechId()?>" target="_blank"><?=$task->getTech()->getName();?>
+			</td>
+			<td>
+				<?=$task->getStatusText(); ?>
+			</td>
+		</tr>
+	<?php endforeach;?>
+</table>
